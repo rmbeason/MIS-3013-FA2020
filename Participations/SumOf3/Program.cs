1e2e1aadb1d16115b556f7c9db54578c0c0739ec
Rachel Beason <rachel.m.beason@ou.edu>
2020-09-02 15:58:43 -0500

    starting code, won't run

--- Participations/SumOf3/Program.cs
+++ Participations/SumOf3/Program.cs
@@ -11,11 +11,23 @@ namespace SumOf3
     {
         const double LUCKY_NUMBER = 7.777;
 
-
-
         static void Main(string[] args)
         {
-            
+
+            string number1input = Console.ReadLine;
+            string number2input = Console.ReadLine;
+            string number3input = Console.ReadLine;
+
+            Console.WriteLine("Please enter your first number >>");
+            number1input = Console.ReadLine();
+
+            Console.WriteLine("Please enter your second number >>");
+            number2input = Console.ReadLine();
+
+            Console.WriteLine("Please enter your third number >>");
+            number3input = Console.ReadLine();
+
+            Console.WriteLine (number1input + number2input + number3input)
         }
     }
 }
