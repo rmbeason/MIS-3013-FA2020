c476d410b15bcce2c4d51cf0c90213fc029eb481
Rachel Beason <rachel.m.beason@ou.edu>
2020-09-10 01:58:51 -0500

    started the conditional fizzbuzz problem

---
+++ Participations/Conditional_FizzBuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Conditional_FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(1, 101);
+
+            // BOOLEAN OPERATORS:
+            //      OR = | |
+            //      AND = &&
+
+            if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            
+            
+           Console.WriteLine(number);
+
+
+           
+        }
+    }
+}
