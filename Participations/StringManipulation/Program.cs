0d79e5c763fbf31592251d4c05e9f8223aaa2d51
Rachel Beason <rachel.m.beason@ou.edu>
2020-09-25 12:13:51 -0500

    update

--- Participations/StringManipulation/Program.cs
+++ Participations/StringManipulation/Program.cs
@@ -11,6 +11,13 @@ namespace StringManipulation
 
             char firstletter = instanceOfString[0];
 
+            Console.WriteLine("Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, " +
+                "and the universe trying to build bigger and better idiots. So far, the universe is winning");
+
+            Console.WriteLine("What word would you like to look for in the above sentence? >>");
+            Console.ReadLine();
+
+
             for (int i = 0; i < 100; i = i + 10)
             {
                 Console.WriteLine(i);
