44339341304b16474760ee46ee052b8b5b2e4eb4
Rachel Beason <rachel.m.beason@ou.edu>
2020-10-06 11:58:19 -0500

    beginning of part csv

---
+++ Participations/ProcessingACSVFile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProcessingACSVFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"sales_data_sample.csv";
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.ReadKey();
+        }
+    }
+}
