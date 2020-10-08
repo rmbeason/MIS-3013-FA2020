573d5ea1334631840910a05fc3b4869b1ea188e1
Rachel Beason <rachel.m.beason@ou.edu>
2020-10-08 10:04:40 -0500

    update on collections

--- Participations/Collections/Program.cs
+++ Participations/Collections/Program.cs
@@ -7,11 +7,37 @@ namespace Collections
   
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter exam grade 1.");
-            Console.WriteLine("Please enter exam grade 2.");
-            Console.WriteLine("Please enter exam grade 3.");
-            Console.WriteLine("Please enter exam grade 4.");
+            // assuming there are four exam grades
 
+            int final = 0, min, max;
+            int[] grades = new int[4]
+            Console.WriteLine("Please enter the student's exam grades >>");
+
+            for (int i = 0; i < 4; i++)
+            {
+                grades[i] = Convert.ToInt32(Console.ReadLine());
+                final += grades[i];
+            }
+
+            min grades[0];
+            max = grades[0];
+
+            for (int i = 1; i < 4; i++)
+            {
+                if (grades[i] < min)
+                {
+                    min = grades[i];
+                }
+                if (grades[i] > max)
+                {
+                    max[i] = grades[i];
+                }
+            }
+
+            Console.WriteLine("Mean score = {0}", final / 4);
+            Console.WriteLine("Minimum score = {0}", min);
+            Console.WriteLine("Max score = {0}", max);
+            Console.ReadLine();
         }
     }
 }
