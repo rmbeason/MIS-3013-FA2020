ebac9c717b8fadb496c0abb6a13cd5d771a77c3c
Rachel Beason <rachel.m.beason@ou.edu>
2020-10-07 23:15:07 -0500

    collections

---
+++ Participations/Collections/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Collections
+{
+    class Program
+    {
+  
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter exam grade 1.");
+            Console.WriteLine("Please enter exam grade 2.");
+            Console.WriteLine("Please enter exam grade 3.");
+            Console.WriteLine("Please enter exam grade 4.");
+
+        }
+    }
+}
