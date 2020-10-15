c32e7403e198ffb0b41b40c8da85dfd85bb5c100
Rachel Beason <rachel.m.beason@ou.edu>
2020-10-14 23:43:52 -0500

    read in file

---
+++ Participations/ProcessingACSV2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProcessingACSV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"SalesJan2009.csv";
+
+        }
+    }
+}
