51d74d933c093ea1bd385f8cde51b2afd1d96181
Rachel Beason <rachel.m.beason@ou.edu>
2020-10-20 12:18:20 -0500

    class changes

---
+++ Participations/Methods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Methods
+{
+    class Program
+    {
+        static List<int> values = new List<int>();
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please enter an animal type. >>");
+            string animalType = Console.ReadLine();
+            string animalSound = Console.ReadKey();
+
+            static string Speak(string animalType);
+            {
+                if ()
+                {
+                    
+                }
+                
+                return animalSound;
+                
+                
+            }   
+
+
+            
+
+
+        }
+    }
+}
