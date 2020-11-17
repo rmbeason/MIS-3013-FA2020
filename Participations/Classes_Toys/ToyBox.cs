da4dc386cc6b3d0264551b3a46ddda6f467e38ee
Rachel Beason <rachel.m.beason@ou.edu>
2020-11-17 12:36:10 -0600

    toy

---
+++ Participations/Classes_Toys/ToyBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Toys
+{
+    class ToyBox
+    {
+
+        public List<Toy> Toys { get; set; }
+
+        public ToyBox()
+        {
+            Toys = new List<Toy>();
+        }
+
+        public Toy GetRandomToy ( )
+        {
+            // generate a random number between 0 and the # of the toys in the Toys property
+
+            //get the toy object at the index of the random # you generated
+            
+            //return the toy from the second step
+        }
+    }
+}
