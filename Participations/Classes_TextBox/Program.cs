f572d477292a66ddb623375d984746c99c788cea
Rachel Beason <rachel.m.beason@ou.edu>
2020-11-30 13:28:01 -0600

    textbox

---
+++ Participations/Classes_TextBox/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Classes_TextBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //textbox class part
+            //textbox class added
+        }
+    }
+}
---
+++ Participations/Classes_TextBox/TextBox.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_TextBox
+{
+    public class TextBox 
+    {
+
+    }
+}
